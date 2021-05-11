784b526434f85970472997d235c5a126b849d441
bukit3point0 <bukit3point0@gmail.com>
2021-05-11 05:25:50 -0400

    middleware added

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,71 @@
+const Accounts = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  console.log(req.body)
+  const {name, budget} = req.body
+  try {
+    if (!name || !budget) {
+      res.status(400).json({
+        message: `name and budget are required`
+      })
+    } else if (typeof name !== string) {
+      res.status(400).json({
+        message: `name of account must be a string`
+      })
+    } else if (name.trim().length() < 3 || name.trim().length > 100) {
+      res.status(400).json({
+        message: `name of account must be between 3 and 100`
+      })
+    } else if (typeof budget !== number) {
+      res.status(400).json({
+        message: `budget of account must be a number`
+      })
+    } else if (budget < 0 || budget > 1000000) {
+      res.status(400).json({
+        message: `budget of account is too large or too small`
+      })
+    } else {
+      next()
+    }
+  } catch(err) {
+    next(err)
+  }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+// - `checkAccountNameUnique` returns a status 400 with a `{ message: "that name is taken" }` if the _trimmed_ `req.body.name` already exists in the database
+
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const accounts = await Accounts.getAll()
+    accounts.filter(account => {
+      if (account.name === req.body.name.trim()) {
+        res.status(400).json({
+          message: `that name is taken`
+        })
+      } else {
+        return account
+      }
+    })
+  } catch (err) {
+    next({
+      message: err.message
+    })
+  }
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const account = await Accounts.getAll(req.params.id)
+    if(!account) {
+      next({
+        status: 400,
+        message: `account not found`
+      })
+    } else {
+      req.account = account
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
